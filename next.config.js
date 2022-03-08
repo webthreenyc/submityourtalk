26053f4bb690eccb2267d98868cfe2777089e0b7
Benjamin Schachter <benschac@gmail.com>
2022-03-08 06:48:54 -0500

    thing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-import { isProd } from "./utils/env";
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
