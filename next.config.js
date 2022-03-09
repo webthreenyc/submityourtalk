2b759c450f4ae52061c441dad5f77602a14649c4
benschac <benschac@gmail.com>
2022-03-09 13:26:34 +0000

    Deploying to gh-pages from @ webthreenyc/submityourtalk@0de3ce4b78fdadf7eb1517279c61dc9c53940bfe 🚀

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-require("dotenv").config();
-module.exports = {
-  reactStrictMode: true,
-  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
-  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : "",
-};
